2a0be86fd0b4b09788ecea47572ab49f796813c7
arvind100691 <arvind100691@gmail.com>
2016-09-19 09:41:23 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,19 +5,79 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var pages = {
+    pageOne: {
+        title:'Page One',
+        date: '16 Sept 2016',
+        heading: 'Page One',
+        content:`
+        <p>
+            Pins may also serve other functions, like Serial, I2C, SPI. These functions are normally activated by the corresponding library. The diagram below shows pin mapping for the popular ESP-12 module.
+        </p>
+        `
+    },
+    pageTwo:{
+        title:'Page Two',
+        date: '17 Sept 2016',
+        heading: 'Page Two',
+        content:`
+        <p>
+           Note that some boards and modules (ESP-12ED, NodeMCU 1.0) also break out pins 9 and 11. These may be used as IO if flash chip works in DIO mode (as opposed to QIO, which is the default one).
+        </p>
+        `
+    },
+    pageThree:{
+        title:'Page Three',
+        date: '16 Sept 2016',
+        heading: 'Page Three',
+        content:`
+        <p>
+            Digital pins 6—11 are not shown on this diagram because they are used to connect flash memory chip on most modules. Trying to use these pins as IOs will likely cause the program to crash.
+        </p>
+        `    
+    }
+};
+
+function createTemplate(data)   {
+    var title = data.title;
+    var heading = data.heading;
+    var date = data.date;
+    var content = data.content;
+    var htmlTemlate = `
+    <html>
+    <head>
+        <title>${title}</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1"/>
+        <link href="/ui/style.css" rel="stylesheet" />
+    </head>
+    <body>
+        <div class="container">
+        <div>
+            <a href="/">Home</a>
+        </div>
+        <hr>
+        <h3>${heading}</h3>
+        <div>
+            ${date}
+        </div>
+        <div>
+            ${content}
+        </div>
+        </div>
+    </body>
+</html>
+`;
+return htmlTemplate;
+}
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/page-one',function(req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'page-one.html'));
+app.get('/:pageName',function(req, res) {
+    var pageName = req.params.pageName;
+  res.send(createTemplate(pages[pageName]));
 });
-app.get('/page-three',function(req, res) {
-   res.send('page three requested'); 
-});
-app.get('/page-two',function(req, res) {
-   res.send('page two requested'); 
-});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
